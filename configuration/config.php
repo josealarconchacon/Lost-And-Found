80a8c48b785184da4cfec361843f2101dffca37f
josealarconchacon <jose.alarcon.chacon@gmail.com>
2023-06-01 12:38:21 -0400

    update file info

--- configuration/config.php
+++ configuration/config.php
@@ -2,7 +2,7 @@
 // Database credentials
 $db_host = "localhost";
 $db_user = "root";
-$db_pass = "Createpass1991";
+$db_pass = "";
 $db_name = "LFDatabase";
 // Connect to DB
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
