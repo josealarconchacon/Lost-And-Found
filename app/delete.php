9c16bb927917fdb176255aed60c8063d4221dd12
josealarconchacon <jose.alarcon.chacon@gmail.com>
2023-06-06 11:46:33 -0400

    Deleting unwanted post

--- app/delete.php
+++ app/delete.php
@@ -1,13 +1,21 @@
 <?php 
 // Initialize the session
 session_start();
-$logged_in_user = $_SESSION['username'];
-$id=$_GET['item_id'];
 // connection
 include_once '../configuration/config.php';
-// Deleting a specific record from the table
-mysqli_query($conn,"delete from `lostItem` where id='$id'");
-// Redirect the user to the display page
-header('location:post.php');
+$logged_in_user = $_SESSION['username'];
+
+if(isset($_GET['deleteId'])) {
+    $id = $_GET['deleteId'];
+    // Deleting a specific record from the table
+    $sql = "delete from `lostItem` where item_id=$id";
+    $result = mysqli_query($conn, $sql);
+    if($result) {
+        header('location: post.php');
+        } else {
+            echo "Error deleting record: " . mysqli_error($conn);
+        }
     
+    // Redirect the user to the display page
+}   
 ?>
